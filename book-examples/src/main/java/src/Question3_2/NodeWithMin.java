d372664f73f38a50ae2798cc5f59ede0a5dc2125
oopchoi <oopchoi@gmail.com>
2013-12-03 20:43:30 +0900

    book 예제 소스

---
+++ book-examples/src/main/java/src/Question3_2/NodeWithMin.java
@@ -0,0 +1,10 @@
+package src.Question3_2;
+
+class NodeWithMin {
+    public int value;
+    public int min;
+    public NodeWithMin(int v, int min){
+        value = v;
+        this.min = min;
+    }
+}

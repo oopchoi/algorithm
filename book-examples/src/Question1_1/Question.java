80ea1071702f6defa43c2c90fa69a195122bf37b
oopchoi <oopchoi@gmail.com>
2013-12-03 22:41:29 +0900

    book 예제 소스

--- book-examples/src/Question1_1/Question.java
+++ book-examples/src/Question1_1/Question.java
@@ -14,7 +14,7 @@ public class Question {
 		}
 		return true;
 	}
-	
+
 	public static boolean isUniqueChars2(String str) {
 		if (str.length() > 256) {
 			return false;
@@ -40,15 +40,28 @@ public class Question {
         StringBuffer description = new StringBuffer();
         description.append("아스키 문자코드의 전체 갯수는 256개 이므로,\n");
         description.append("입력 받는 문자열 갯수가 256개가 넘으면\n");
-        description.append("중복되는 문자가 존재한다는 의미이기 때문에 곧 바로 return 한다.");
-        System.out.println("---------------------------- 해설 시작 -----------------------");
-        System.out.println(description);
-        System.out.println("---------------------------- 해설 끝 ------------------------");
+        description.append("중복되는 문자가 존재한다는 의미이기 때문에 곧 바로 return 한다.\n\n");
 
-		String[] words = {"abcde", "hello", "apple", "kite", "padle"};
+        description.append("검증) 입력 받은 문자열의 갯수가 256개가 넘으면 false를 반환한다.\n");
+        description.append("1. 256개의 boolean 배열을 생성한다.\n");
+        description.append("2. 입력 받은 문자열의 길이만큼 루프를 돌면서 각 문자를 int형의 아스키코드로 변환한다.\n");
+        description.append("3. 아스키코드를 인덱스로 사용하여 해당하는 boolean 배열 요소를 검사하여 true로 마킹되어 있으면 false를 반환한다.\n");
+        description.append("4. 아스키코드를 인덱스로 사용하여 해당하는 boolean 배열 요소에 true를 마킹한다.");
+        System.out.println("---------------------------- 해설 시작 (해법 1) -----------------------");
+        System.out.println(description);
+        System.out.println("---------------------------- 해설 끝 (해법 1) ------------------------");
+
+        System.out.println("---------------------------- 코드 실행 시작 (해법 1) -----------------------");
+        String[] words = {"abcde", "hello", "apple", "kite", "padle"};
+        for (String word : words) {
+            System.out.println(word + ": " + isUniqueChars2(word));
+        }
+        System.out.println("---------------------------- 코드 실행 끝 (해법 1) -----------------------");
+
+		/*String[] words = {"abcde", "hello", "apple", "kite", "padle"};
 		for (String word : words) {
 			System.out.println(word + ": " + isUniqueChars(word) + " " + isUniqueChars2(word));
-		}
+		}*/
 	}
 
 }

57a5945d9eec7280dfa47a08d384cd0db680b13a
oopchoi <oopchoi@gmail.com>
2013-12-03 20:50:42 +0900

    book 예제 소스

---
+++ book-examples/src/main/java/src/Question10_7/Node.java
@@ -0,0 +1,13 @@
+package src.Question10_7;
+
+public class Node {
+	public Node prev;
+	public Node next;
+	public String[] results;
+	public String query;
+
+	public Node(String q, String[] res) {
+		results = res;
+		query = q;
+	}
+}

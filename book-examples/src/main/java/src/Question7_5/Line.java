802d8574363bddf497e583f5b32eae979c6d7069
oopchoi <oopchoi@gmail.com>
2013-12-03 20:44:06 +0900

    book 예제 소스

---
+++ book-examples/src/main/java/src/Question7_5/Line.java
@@ -0,0 +1,14 @@
+package src.Question7_5;
+
+public class Line {
+	public Point start;
+	public Point end;
+	public Line(Point start, Point end) {
+		this.start = start;
+		this.end = end;
+	}
+	
+	public String toString() {
+		return "Line from " + start.toString() + " to " + end.toString();
+	}
+}

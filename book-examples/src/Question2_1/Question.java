c59cabae3829a1f2bd86c15a5635d24eb6fc1e1a
oopchoi <oopchoi@gmail.com>
2013-12-05 18:31:56 +0900

    Update Question.java

--- book-examples/src/Question2_1/Question.java
+++ book-examples/src/Question2_1/Question.java
@@ -2,7 +2,6 @@ package Question2_1;
 
 import java.util.Hashtable;
 
-import CtCILibrary.AssortedMethods;
 import CtCILibrary.LinkedListNode;
 
 public class Question {
@@ -84,14 +83,43 @@ public class Question {
 		}
 	}	
 	
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
+        StringBuffer problem = new StringBuffer();
+        problem.append("다음의 비정렬 연결 리스트에서 중복 문자를 제거하는 코드를 작성하라.\n");
+        problem.append("연관문제 : 임시 버퍼가 허용되지 않는 상황에서 이 문제를 어떻게 해결할 수 있겠는가?");
+        System.out.println("---------------------------- 문제 시작 -----------------------");
+        System.out.println(problem);
+        System.out.println("---------------------------- 문제 끝 ------------------------");
+        StringBuffer description = new StringBuffer();
+        description.append("연결 리스트에서 중복을 제거하기 위해서는 중복을 추적할 필요가 있다.\n");
+        description.append("단순히 연결 리스트를 순회하면서 각 원소를 해시 케이블에 저장하고\n");
+        description.append("그러다 중복을 발견하면, 그 원소는 제거하고 계속 진행한다.\n");
+        description.append("이 알고리즘은 O(N) 시간에 수행된다.");
+        System.out.println("---------------------------- 해설 시작 (해법 1) -----------------------");
+        System.out.println(description);
+        System.out.println("---------------------------- 해설 끝 (해법 1) ------------------------");
+
+        System.out.println("---------------------------- 코드 실행 시작 (해법 1) -----------------------");
 		LinkedListNode first = new LinkedListNode(0, null, null); //AssortedMethods.randomLinkedList(1000, 0, 2);
 		LinkedListNode head = first;
 		LinkedListNode second = first;
-		for (int i = 1; i < 8; i++) {
-			second = new LinkedListNode(i % 2, null, null);
-			first.setNext(second);
+        System.out.println(first.data + ", " + head.data + ", " + second.data);
+        for (int i = 1; i < 8; i++) {
+			second = new LinkedListNode(i, null, null);
+            System.out.println("second = new LinkedListNode(" + i + ")");
+            System.out.println("i ("+ i + "), h (" + head + ")");
+            System.out.println("i ("+ i + "), f (" + first + ")");
+            System.out.println("i ("+ i + "), s (" + second + ")");
+            first.setNext(second);
+            System.out.println("first.setNext(second)");
+            System.out.println("i ("+ i + "), h (" + head + ")");
+            System.out.println("i ("+ i + "), f (" + first + ")");
+            System.out.println("i ("+ i + "), s (" + second + ")");
 			second.setPrevious(first);
+            System.out.println("second.setPrevious(first)");
+            System.out.println("i ("+ i + "), h (" + head + ")");
+            System.out.println("i ("+ i + "), f (" + first + ")");
+            System.out.println("i ("+ i + "), s (" + second + ")");
 			first = second;
 		}
 		System.out.println(head.printForward());
@@ -100,5 +128,6 @@ public class Question {
 		deleteDupsC(clone);
 		System.out.println(tapB);
 		System.out.println(tapC);
+        System.out.println("---------------------------- 코드 실행 끝 (해법 1) -----------------------");
 	}
 }
